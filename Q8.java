474988b3abb4537e2f940d1932a73365b15cb644
Aabhisek <72724849+Aabhisek@users.noreply.github.com>
2021-06-05 21:02:53 +0530

    Add files via upload

---
+++ Q8.java
@@ -0,0 +1,11 @@
+public class Qn8 {
+
+	public static void main(String[] args) {
+		
+		 float x=-7/5;
+		 
+		 System.out.println("the solution of the eqn -2*2=5X+3 is "+x);
+	}
+
+}
+© 2021 GitHub, Inc.
